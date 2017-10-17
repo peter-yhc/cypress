a86c925b5fd06df0b15d4c93e54748b6a5ab0bdd
Peter Chen <peter.chen2@mail.mcgill.ca>
2017-10-17 12:44:54 +1100

    Added basic components for app table and app graph.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { MaterializeModule } from 'angular2-materialize';
+import {MaterializeModule} from 'angular2-materialize';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {AccountOverviewComponent} from './account-overview/account-overview.component';
+import {DashboardGraphComponent} from './dashboard-graph/dashboard-graph.component';
+
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AccountOverviewComponent,
+        DashboardGraphComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/dashboard-graph/dashboard-graph.component.ts
@@ -0,0 +1,10 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-dashboard-graph',
+    templateUrl: 'dashboard-graph.component.html'
+})
+export class DashboardGraphComponent {
+
+}
