cb4eb607bac6ed64111933b48e7c9689a6d98670
Peter Chen <peter.chen2@mail.mcgill.ca>
2017-10-26 23:27:33 +1100

    Change tslint to allow trailing whitespace. Added empty test for account overview.

---
+++ src/app/account-overview/account-overview.component.spec.ts
@@ -0,0 +1,17 @@
+import {TestBed} from '@angular/core/testing';
+import {AccountOverviewComponent} from './account-overview.component';
+
+describe('AppComponent', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AccountOverviewComponent
+            ],
+        }).compileComponents();
+    });
+
+    it('component should be loaded with ticker exchanges', () => {
+        
+    });
+
+});
