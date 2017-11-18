bd2a4dd2ddf254b3ed9421cea87027010811f74a
Peter Chen <peter.chen2@mail.mcgill.ca>
2017-11-18 21:52:40 +1100

    Remove tab view, and create new account package.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import {TestBed, async} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {DashboardGraphComponent} from './dashboard-graph/dashboard-graph.component';
-import {AccountOverviewComponent} from './account-overview/account-overview.component';
+import {AccountOverviewComponent} from './account/account-overview/account-overview.component';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
@@ -27,6 +27,6 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to Cypress');
+        expect(compiled.querySelector('.brand-logo').textContent).toContain('Cypress');
     }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {AccountOverviewComponent} from './account-overview/account-overview.component';
+import {AccountOverviewComponent} from './account/account-overview/account-overview.component';
 import {DashboardGraphComponent} from './dashboard-graph/dashboard-graph.component';
 
 
