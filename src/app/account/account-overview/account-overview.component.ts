374d386ddc28cfef199f463d463ecba769eea684
Peter Chen <peter.chen2@mail.mcgill.ca>
2017-11-21 10:08:04 +1100

    Added some styles and test account detail mocks.

--- src/app/account/account-overview/account-overview.component.ts
+++ src/app/account/account-overview/account-overview.component.ts
@@ -4,9 +4,10 @@ import {InvestmentAccount} from '../../model/InvestmentAccount';
 
 @Component({
     selector: 'app-account-overview',
-    templateUrl: 'account-overview.component.html'
+    templateUrl: 'account-overview.component.html',
+    styleUrls: ['account-overview.component.css']
 })
 export class AccountOverviewComponent {
 
-    investmentAccount = new InvestmentAccount();
+    investmentAccount = new InvestmentAccount('Test Account 1', 23400.01);
 }
